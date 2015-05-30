a65f2994694a449fdfa0dc594a7c6d76c2b15793
Jan Melcher <mail@jan-melcher.de>
2015-05-30 23:05:09 +0200

    Only call superusers script if superusers are specified
    (the script fails with sentry 7.*)

--- definitions/sentry_conf.rb
+++ definitions/sentry_conf.rb
@@ -164,31 +164,33 @@ define :sentry_conf,
   end
 
   # # Create superusers
-  template node["sentry"]["superuser_creator_script"] do
-    owner params[:user]
-    group params[:group]
-
-    source "sentry/superuser_creator.py.erb"
-    variables(:config => config,
-              :superusers => params[:superusers] || node["sentry"]["superusers"],
-              :virtualenv => virtualenv_dir)
-    cookbook params[:templates_cookbook]
-  end
-
-  # # sentry --config=/etc/sentry.conf.py createsuperuser
-  bash "create sentry superusers" do
-    user params[:user]
-    group params[:group]
-
-    code <<-EOH
-  . #{virtualenv_dir}/bin/activate &&
-  #{virtualenv_dir}/bin/python #{node['sentry']['superuser_creator_script']} &&
-  deactivate
-  EOH
-  end
-
-  file node['sentry']['superuser_creator_script'] do
-    action :delete
+  unless params[:superusers].empty?
+    template node["sentry"]["superuser_creator_script"] do
+      owner params[:user]
+      group params[:group]
+  
+      source "sentry/superuser_creator.py.erb"
+      variables(:config => config,
+                :superusers => params[:superusers] || node["sentry"]["superusers"],
+                :virtualenv => virtualenv_dir)
+      cookbook params[:templates_cookbook]
+    end
+  
+    # # sentry --config=/etc/sentry.conf.py createsuperuser
+    bash "create sentry superusers" do
+      user params[:user]
+      group params[:group]
+  
+      code <<-EOH
+    . #{virtualenv_dir}/bin/activate &&
+    #{virtualenv_dir}/bin/python #{node['sentry']['superuser_creator_script']} &&
+    deactivate
+    EOH
+    end
+  
+    file node['sentry']['superuser_creator_script'] do
+      action :delete
+    end
   end
 
 end
